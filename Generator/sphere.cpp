1c77075b4cc552a5ce49356c02dcff3f8b72ffdd
45Yoda <ninjadinosaur.ilikecake@gmail.com>
2018-03-11 21:29:48 +0000

    vai bola

--- Generator/sphere.cpp
+++ Generator/sphere.cpp
@@ -14,32 +14,32 @@ std::vector<Vertex*> sphere(float radius, int slices, int stacks) {
     std::vector<Vertex*> vertexes;
     int i, j;
     float angleH = 0, angleV = 0;
-    float shiftH = M_PI / stacks;
-    float shiftV = (2 * M_PI) / slices;
+    float shiftV = M_PI / stacks;
+    float shiftH = (2 * M_PI) / slices;
     float x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4;
 
     for (i = 0; i < stacks; i++) {
-        angleH = shiftH * i;
+        angleV = shiftV * i;
 
         for (j = 0; j < slices; j++) {
 
-            angleV = shiftV * j;
+            angleH = shiftH * j;
 
-            x1 = radius * sin(angleV) * sin(angleH);
-            y1 = radius * cos(angleH);
-            z1 = radius * sin(angleH) * cos(angleV);
+            x1 = radius * sin(angleH) * sin(angleV);
+            y1 = radius * cos(angleV);
+            z1 = radius * sin(angleV) * cos(angleH);
 
-            x2 = radius * sin(angleH) * sin(angleV + shiftV);
-            y2 = radius * cos(angleH);
-            z2 = radius * sin(angleH) * cos(angleV + shiftV);
+            x2 = radius * sin(angleV) * sin(angleH + shiftH);
+            y2 = radius * cos(angleV);
+            z2 = radius * sin(angleV) * cos(angleH + shiftH);
 
-            x3 = radius * sin(angleH + shiftH) * sin(angleV + shiftV);
-            y3 = radius * cos(angleH + shiftH);
-            z3 = radius * sin(angleH + shiftH) * cos(angleV + shiftV);
+            x3 = radius * sin(angleV + shiftV) * sin(angleH + shiftH);
+            y3 = radius * cos(angleV + shiftV);
+            z3 = radius * sin(angleV + shiftV) * cos(angleH + shiftH);
 
-            x4 = radius * sin(angleH + shiftH) * sin(angleV);
-            y4 = radius * cos(angleH + shiftH);
-            z4 = radius * sin(angleH + shiftH) * cos(angleV);
+            x4 = radius * sin(angleV + shiftV) * sin(angleH);
+            y4 = radius * cos(angleV + shiftV);
+            z4 = radius * sin(angleV + shiftV) * cos(angleH);
 
 
             vertexes.push_back(new Vertex(x1, y1, z1));
